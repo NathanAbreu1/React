eba11a630dbfebf86238d7261dadb89c51817859
NathanAbreu1 <Nathan11.abreu@gmail.com>
2023-02-10 21:49:10 -0300

    finishing the card

--- Project Personal Card/src/Components/Main.js
+++ Project Personal Card/src/Components/Main.js
@@ -6,7 +6,7 @@ export default function Main(){
   <div className="content">
     <div className="info-container">
       <h2>Nathan Abreu</h2>
-      <p>Software Engineering</p>
+      <p>Software Engineer</p>
       <div className="info-container-links">
         <a className="email"><Icon icon="material-symbols:mark-email-read-outline" className="Icon"/>Email</a>
         <a className="linkedin"><Icon icon="ph:linkedin-logo" className="Icon"/>Linkedin</a>
