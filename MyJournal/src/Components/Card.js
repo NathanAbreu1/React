dd21ab821fd139460497e04bf657d4e1e1aaa5d0
NathanAbreu1 <Nathan11.abreu@gmail.com>
2023-02-24 19:51:02 -0300

    Finishing myjournal

--- MyJournal/src/Components/Card.js
+++ MyJournal/src/Components/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Icon } from '@iconify/react';
 
 export default function Card(props){
 
@@ -8,10 +9,13 @@ export default function Card(props){
       <img src={props.item.img}></img>
       </div>
       <div className="content">
-        <p>{props.item.location}</p>
-        <a href={props.item.link}></a>
+        <div className="location">
+        <Icon id = "icon" icon="material-symbols:location-on" width="25px" height="25px"/>
+         <h2>{props.item.location}</h2>
+         <a href={props.item.link}>View on Google Maps</a>
+        </div>
         <h1>{props.item.title}</h1>
-        <p>{props.item.date}</p>
+        <h2>{props.item.date}</h2>
         <p>{props.item.text}</p>
       </div>
     </div>
--- MyJournal/src/Data.js
+++ MyJournal/src/Data.js
@@ -1,29 +1,29 @@
 export default [
   {
     id: 1,
-    location: "Japan",
-    link: "to fill",
+    location: "JAPAN",
+    link: "https://www.google.com/maps/place/Mount+Fuji/@35.3606255,138.7273634,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipN0UpdkyHAkhhuxpTN6z11XVUCQJeeH9Yba-z3I!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipN0UpdkyHAkhhuxpTN6z11XVUCQJeeH9Yba-z3I%3Dw129-h86-k-no!7i3456!8i2304!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA",
     title: "Mount Fuji",
     img: "https://source.unsplash.com/WLxQvbMyfas",
-    date: "12 Jan 2021 - 24 Jan 2021",
+    date: "12 Jan, 2021 - 24 Jan, 2021",
     text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
   },
   {
     id: 2,
-    location: "Sidney",
-    link: "to fill",
-    title: "Mount Fuji",
+    location: "AUSTRALIA",
+    link: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567799,151.213108,17z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m6!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0",
+    title: "Sidney Opera House",
     img: "https://source.unsplash.com/JmuyB_LibRo",
-    date: "12 Jan 2021 - 24 Jan 2021",
-    text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    date: "27 May, 2021 - 8 Jun, 2021",
+    text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
   },
   {
     id: 3,
-    location: "Norway",
-    link: "to fill",
-    title: "Mount Fuji",
+    location: "NORWAY",
+    link: "https://www.google.com/maps/place/Geirangerfjord/@62.1049113,7.0051741,12z/data=!3m1!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi",
+    title: "Geirangerfjord",
     img: "https://source.unsplash.com/3PeSjpLVtLg",
-    date: "12 Jan 2021 - 24 Jan 2021",
-    text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+    date: "01 Oct, 2021 - 18 Nov, 2021",
+    text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
   }
 ]
