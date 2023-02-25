c0471b052f2166fc023903dc59a435ee568511ee
NathanAbreu1 <Nathan11.abreu@gmail.com>
2023-02-25 14:20:02 -0300

    Starting meme generator

---
+++ MemeGenerator/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Header(){
+
+  return(
+    <div className="header">
+      <div className="logo">
+        <img src="../images/troll.png"></img>
+        <h1>Meme Generator</h1>
+        </div>
+      <div className="items">
+        <p>React-Course-Project 3</p>
+      </div>
+    </div>
+  )
+}
